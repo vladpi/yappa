d7f3be2bcd9174b39abccc6261ec87c3bb884882
Egor Korovin <turokg@icloud.com>
2021-07-14 21:40:11 +0700

    lowered python version to 3.8, added flask example

---
+++ examples/flask/flask.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return 'root url'
+
+
+@app.route('/json')
+def json():
+    return {
+        "result": "json",
+        "sub_result": {"sub": "json"}
+    }
+
+
+@app.route("/query_params")
+def query_params():
+    return {"params": request.args}
+
+
+@app.route("/url_param/<param>")
+def url_param(param):
+    return {"param": param}
+
+
+@app.route("/post/", methods=["POST"])
+def post():
+    return {"request": request.get_json(force=True)}
