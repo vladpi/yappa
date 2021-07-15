b46cdf352cf4bdfa04cbd68e1d6f0e2a818ff033
Egor Korovin <turokg@icloud.com>
2021-07-15 22:34:26 +0700

    altered including yaml filen

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
         name='yappa',
-        version='0.4.4',
+        version='0.4.5',
         url='https://github.com/turokg/yappa',
         description='Easy serverless deploy of python web applications',
         long_description_content_type="text/markdown",
@@ -27,6 +27,6 @@ setup(
         python_requires='>=3.8.0',
         entry_points={'console_scripts': ['yappa = yappa.cli:cli']},
         license="MIT",
-        package_data={'yappa': ['yappa.yaml', 'yappa-gw.yaml']},
+        package_data={'yappa': ['*.yaml']},
         include_package_data=True,
         )
