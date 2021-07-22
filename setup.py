6df30dfe4d5e6fb880ac3f176a099ae77956c792
Egor Korovin <turokg@yahoo.com>
2021-07-22 21:35:19 +0400

    upped package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
         name='yappa',
-        version='0.4.10',
+        version='0.4.11',
         url='https://github.com/turokg/yappa',
         description='Easy serverless deploy of python web applications',
         long_description_content_type="text/markdown",
