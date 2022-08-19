928dbd87ad66ff214fc575a74eb0a78b060e480e
Egor Korovin <eyukorovin@avito.ru>
2022-08-19 16:30:53 +0700

    workaroud to skip validation on Windows platform

--- yappa/packaging/common.py
+++ yappa/packaging/common.py
@@ -1,9 +1,13 @@
+import platform
+
 from click import ClickException
 
 REQUIRED_PACKAGES = ("httpx", "PyYAML")
 
 
 def validate_requirements_file(requirements_filename):
+    if any(platform.win32_ver()):
+        return
     try:
         with open(requirements_filename, encoding="utf-8") as f:
             requirements = f.read()
