a0325dc96e5c418407c8c089ee895abe0678e5c4
Egor Korovin <eyukorovin@avito.ru>
2023-07-27 16:37:22 +0300

    add jinja 2 example

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from jinja2 import FileSystemLoader, Environment
 
 app = Flask(__name__)
+
+env = Environment(loader=FileSystemLoader("templates"))
 
 
 @app.route('/')
@@ -9,7 +12,8 @@ def main():
             '<img src="https://github.com/turokg/yappa/raw/master/logo.png"/>'
             '<h1>Hello from Yappa! </h1>'
             "It's root url"
-            '<div><a href="/json">Go to json API path</a></div>'
+            '<div><a href="/json">Go to json API</a></div>'
+            '<div><a href="/jinja">Go to Jinja2 rendered html</a></div>'
             '</center>'
             )
 
@@ -20,3 +24,9 @@ def json():
         "hello": "from Yappa!",
         "path": "json",
     }
+
+@app.route('/jinja')
+def jinja():
+    template = env.get_template("sample.html")
+    return template.render({"message": "Hello world! -- from jinja"})
+
